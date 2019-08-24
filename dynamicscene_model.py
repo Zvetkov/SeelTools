a54e8c6cfb037eb2af917a37b87beecd29615022
Zvetkov <seel.ru@gmail.com>
2019-08-25 01:24:34 +0300

    Implement dynamicscene formating parity with original game files

--- dynamicscene_model.py
+++ dynamicscene_model.py
@@ -5,11 +5,13 @@ from lxml import objectify
 
 DYNAMIC_SCENE_XML = os.path.join(os.path.dirname(__file__), "dummy_files", "dynamicscene.xml")
 DYNAMIC_SCENE_CHANGED = os.path.join(os.path.dirname(__file__), "dummy_files", "dynamicscenemod.xml")
+DYNAMIC_SCENE_RAW = os.path.join(os.path.dirname(__file__), "dummy_files", "dynamicsceneraw.xml")
 
 
 def main():
     dynamic_scene_tree = parse_file_to_tree(DYNAMIC_SCENE_XML)
     save_to_file(dynamic_scene_tree, DYNAMIC_SCENE_CHANGED)
+    save_to_file(dynamic_scene_tree, DYNAMIC_SCENE_RAW, False)
 
 
 def parse_file_to_tree(path):
@@ -21,39 +23,78 @@ def parse_file_to_tree(path):
     return objectify_tree
 
 
-def save_to_file(objectify_tree: objectify.ObjectifiedElement, path):
+def save_to_file(objectify_tree: objectify.ObjectifiedElement, path, machina_beautify: bool = True):
+    '''
+    Saves ObjectifiedElement tree to file at path, will format and beautify file
+    in the style very similar to original Ex Machina dynamicscene.xml files by
+    default. Can skip beautifier and save raw lxml formated file.
+    '''
     xml_string = etree.tostring(objectify_tree,
                                 pretty_print=True,
                                 xml_declaration=True,
                                 encoding='windows-1251',
                                 standalone=True)
     with open(path, "wb") as writer:
-        writer.write(xml_attributes_to_new_line(xml_string))
+        if machina_beautify:
+            writer.write(machina_xml_beautify(xml_string))
+        else:
+            writer.write(xml_string)
 
 
-def xml_attributes_to_new_line(xml_string):
-    string = b""
-    for i, line in enumerate(xml_string.splitlines()):
+def machina_xml_beautify(xml_string: str):
+    ''' Format and beautify xml string in the style very similar to original
+    Ex Machina dynamicscene.xml files.'''
+    beautified_string = b""
+    previous_line_indent = -1
+
+    # As first line of xml file is XML Declaration, we want to exclude it from
+    # Beautifier to get rid of checks for every line that it's not a Declaration
+    xml_string_first_line = xml_string[:xml_string.find(b"\n<")]
+
+    for i, line in enumerate(xml_string[xml_string.find(b"\n<") + 1:].splitlines()):
         line_stripped = line.lstrip()
+        # calculating indentation level of parent line to indent its attributes.
+        # lxml use spaces for indents but EM files use tabs, so indents maps 2:1
         line_indent = (len(line) - len(line_stripped)) // 2
-        line = split_tag_on_attributes(line_stripped, line_indent)
-        string += line_indent * b"\t" + line + b"\n"
-        # print(line_indent * b"\t" + line + b"\n")
-    return string
+
+        line = _split_tag_on_attributes(line_stripped, line_indent, previous_line_indent)
+        # manually tabulating lines according to saved indent level
+        line = line_indent * b"\t" + line + b"\n"
+
+        # in EM xmls every first and only first tag of its tree level is
+        # separated by a new line
+        if line_indent == previous_line_indent:
+            line = b"\n" + line
+
+        # we need to know indentation of previous tag to decide if tag is first
+        # for its tree level, as described above
+        previous_line_indent = line_indent
+
+        beautified_string += line
+    return xml_string_first_line + b"\n" + beautified_string
 
 
-def split_tag_on_attributes(xml_line: str, line_indent: int):
+def _split_tag_on_attributes(xml_line: str, line_indent: int,
+                             previous_line_indent: int):
     white_space_index = xml_line.find(b" ")
     quotmark_index = xml_line.find(b'"')
-    if white_space_index == -1 and quotmark_index == -1 and xml_line != b">" and xml_line.find(b"<") == -1:
-        return xml_line + b"\n"
-    elif white_space_index == -1 or quotmark_index == -1:
+
+    # true when no tag attribute contained in string
+    if white_space_index == -1 or quotmark_index == -1:
         return xml_line
+
+    # next tag attribute found
     elif white_space_index < quotmark_index:
-        return xml_line[:white_space_index] + b"\n" + b"\t" * (line_indent + 1) + split_tag_on_attributes(xml_line[white_space_index + 1:], line_indent)
+        # indent found attribute and recursively start work on next line part
+        return (xml_line[:white_space_index] + b"\n" + b"\t" * (line_indent + 1)
+                + _split_tag_on_attributes(xml_line[white_space_index + 1:],
+                                           line_indent, previous_line_indent))
+    # searching where attribute values ends and new attribute starts
     else:
         second_quotmark_index = xml_line.find(b'"', quotmark_index + 1) + 1
-        return xml_line[:second_quotmark_index] + split_tag_on_attributes(xml_line[second_quotmark_index:], line_indent)
+        return (xml_line[:second_quotmark_index]
+                + _split_tag_on_attributes(xml_line[second_quotmark_index:],
+                                           line_indent, previous_line_indent))
 
 
 if __name__ == "__main__":
