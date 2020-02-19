a48d42457440be58e5e7deab6c4a39f4e754b140
Zvetkov <seel.ru@gmail.com>
2020-02-19 09:17:25 +0300

    New class stucture representation based on actual game classes

--- game_classes.py
+++ game_classes.py
@@ -3,146 +3,112 @@ from warnings import warn
 from lxml import objectify
 
 
-# Base class
 class Object(object):
-    def create(self, name: str, prototype: str):
+    '''Base class'''
+    def __init__(self, name: str, prototype: str):
         self.name = name
         self.prototype = prototype
         self.tag_name = None
 
-    def parse_obj(self, element: objectify.ObjectifiedElement,
-                  dicts: dict = {}):
-        self.prototype = element.attrib["Prototype"]
-        self.tag_name = element.tag
-        self.name = element.attrib.get("Name")
-        if self.name is None:
-            self.name = "OBJECT_MISSING_NAME"
 
-
-# Base class with belong
 class Obj(Object):
-    def create(self, name: str, prototype: str, belong: int):
-        Object.create(self, name, prototype)
-        self.belong = str(belong)
-
-    def parse_obj(self, element: objectify.ObjectifiedElement,
-                  dicts: dict = {}):
-        Object.parse_obj(self, element, dicts)
-        self.belong = element.attrib.get("Belong")  # not required, example chests on r1m2
+    '''Base class with belong'''
+    def __init__(self, name: str, prototype: str,
+                 belong: int = 1000, flags: int = 17,
+                 parent_repository: GeomRepository = None):  # what is this magic 17 ???
+        Object.__init__(self, name, prototype)
+        self.belong = belong
+        self.flags = flags
+        self.parent_repository = parent_repository
 
 
 class GeomRepository(Object):
-    def create(self, name: str, prototype: str, belong: int,
-               items: list):
-        Object.create(self, name, prototype)
+    '''Repository of items'''
+    def __init__(self, name: str, prototype: str, belong: int,
+                 size_x: int = 1, size_y: int = 1, items: list = []):
+        Object.__init__(self, name, prototype)
         self.items = items
+        self.size_x = size_x
+        self.size_y = size_y
+        # self.sort_style
+        # self.vehicle
 
-    def parse_obj(self, element: objectify.ObjectifiedElement,
-                  dicts: dict = {}):
-        Object.parse_obj(self, element, dicts)
-        if element["Repository"].attrib:
-            self.items = [Ware(part_element, dicts)
-                          for part_element in element["Repository"]["Item"]]
+
+class GeomRepositoryItem(object):
+    def __init__(self, origin_x: int = -1, origin_y: int = -1,
+                 amount: int = 0, item_type: int = 1):
+        self.origin_x = origin_x
+        self.origin_y = origin_y
+        self.amount = amount
+        self.item_type = item_type
+
+
+class Player(Obj):
+    '''Player class (singleton?)'''
+    def __init__(self, name: str, prototype: str, belong: int,
+                 money: int, vehicle: Vehicle = None):
+        Obj.__init__(self, name, prototype)
+        self.money = money
+        self.vehicle = vehicle
+        # self.model_name
+        # self.skin_number
+        # self.cfg_number
 
 
 class PhysicObj(Obj):
-    def create(self, name: str, prototype: str, belong: int,
-               position: str, rotation: str, skin: int):
-        Obj.create(self, name, prototype, belong)
+    '''Obj that can be spawned in game worls'''
+    def __init__(self, name: str, prototype: str, belong: int,
+                 position: str, rotation: str, skin: int):
+        Obj.__init__(self, name, prototype, belong)
         self.position = position
         self.rotation = rotation
         self.skin = skin  # ??? might be optional
 
-    def parse_obj(self, element: objectify.ObjectifiedElement,
-                  dicts: dict = {}):
-        Obj.parse_obj(self, element, dicts)
-        self.position = element.attrib["Pos"]  # "1263.873 308.000 2962.220"
-        self.rotation = element.attrib.get("Rot")  # "0.000 -0.721 0.000 -0.693"
+
+class PhysicBody(Obj):
+    '''Base class for VehiclePart and SimplePhysicBody'''
+    def __init__(self, name: str, prototype: str, belong: int,
+                 position: str, rotation: str, skin: int, logo: int):
+        Obj.__init__(self, name, prototype, belong)
+        self.position = position
+        self.rotation = rotation
+        self.skin = skin
+        self.logo = logo
 
 
 class Ware(Obj):
-    def create(self, name: str, prototype: str, belong: int,
-               pos_x: int, pos_y: int, flags: int,
-               prototype: str, prototype_name: str):
-        Obj.create(self, name, prototype, belong)
+    '''Ware than can be sold at the shop'''
+    def __init__(self, name: str, prototype: str, belong: int,
+                 pos_x: int, pos_y: int, flags: int,
+                 prototype_name: str, durability: int,
+                 max_durability: int, price: int,
+                 damage: int, firing_rate: int,
+                 firing_range: int):
+        Obj.__init__(self, name, prototype, belong)
         self.pos_xy = [pos_x, pos_y]
         self.flags = flags
-        self.prototype = prototype
         self.prototype_name = prototype_name
-
-    def parse_obj(self, element: objectify.ObjectifiedElement,
-                  dicts: dict = {}):
-        Obj.parse_obj(self, element, dicts)
-        self.pos_xy = [element.attrib["PosX"], element.attrib["PosY"]]  # [0, 0]
-        self.flags = element.attrib.get("Flags")  # 16 # ??? is this class really working if missing? example: zoo.ssl
-        self.prototype = element.attrib["Prototype"]  # "bugCargo02"
-        self.prototype_name = dicts['model_names'][self.prototype]['value']
-
-        # zoo attribs unused in main game
-        self.durability = element.attrib.get("Durability")  # ??? check that it works as exoected, only available on zoo
-        self.max_durability = element.attrib.get("MaxDurability")  # ??? same as durability
-        self.price = element.attrib.get("Price")  # ??? same as durability
-        self.damage = element.attrib.get("Damage")  # ??? same as durability
-        self.firing_rate = element.attrib.get("FiringRate")  # ??? same as durability
-        self.firing_range = element.attrib.get("FiringRange")  # ??? same as durability
 
 
 class ComplexPhysicObj(PhysicObj):
-    def create(self, name: str, prototype: str, belong: int,
-               position: str, rotation: str, skin: int):
-        PhysicObj.create(self, name, prototype, belong,
-                         position, rotation, skin)
-        pass  # ??? what is the difference between Complex and SimplePO
-
-    def parse_obj(self, element: objectify.ObjectifiedElement,
-                  dicts: dict = {}):
-        PhysicObj.parse_obj(self, element, dicts)
+    '''Physic object that can be composed of multiple parts'''
+    def __init__(self, name: str, prototype: str, belong: int,
+                 position: str, rotation: str, skin: int):
+        PhysicObj.__init__(self, name, prototype, belong,
+                           position, rotation, skin)
         pass  # ??? what is the difference between Complex and SimplePO
 
 
 class Vehicle(ComplexPhysicObj):
-    def create(self, name: str, prototype: str, belong: int,
-               position: str, rotation: str, skin: int,
-               cabin: VehiclePart, basket: VehiclePart, chassis: VehiclePart,
-               parts: VehiclePart, repository: GeomRepository):
-        pass
-
-    def parse_obj(self, element: objectify.ObjectifiedElement,
-                  dicts: dict = {}):
-        self.cabin = None
-        self.basket = None
-        self.chassis = None
-        self.parts = {}
-        for part in element["Parts"].iterchildren():
-            if part.tag == "CABIN":
-                self.cabin = VehiclePartClass(part, dicts)
-            elif part.tag == "BASKET":
-                self.basket = VehiclePartClass(part, dicts)
-            elif part.tag == "CHASSIS":
-                self.chassis = VehiclePartClass(part, dicts)
-            else:
-                self.parts[part.tag] = VehiclePartClass(part, dicts)
-
-        if element["Repository"].attrib:
-            self.repository = GeomRepository().parse_obj(
-                               [SoldPartClass(part_element, dicts)
-                               for part_element in element["Repository"]["Item"]])
-        else:
-            self.repository = None
-
-
-class Player(Obj):
+    '''Vehicle that player of bots can use'''
     def __init__(self, name: str, prototype: str, belong: int,
-                 money: int, vehicle: Vehicle):
-        Obj.__init__(self, name, prototype)
-        self.belong = str(belong)
-        self.money = str(money)
-
-    def parse_obj(self, element: objectify.ObjectifiedElement,
-                  dicts: dict = {}):
-        Obj.parse_obj(self, element, dicts)
-        self.money = element.attrib.get("Money")
-        if len(element.getchildren()) == 1:
-            self.vehicle = Vehicle().parse_obj(element.getchildren()[0], dicts)
-        else:
-            self.vehicle = None  # r3m1 player prototype as example
+                 position: str, rotation: str, skin: int,
+                 cabin: VehiclePart, basket: VehiclePart, chassis: VehiclePart,
+                 parts: VehiclePart, repository: GeomRepository):
+        ComplexPhysicObj.__init__(self, name, prototype, belong,
+                                  position, rotation, skin)
+        self.cabin = cabin
+        self.basket = basket
+        self.chassis = chassis
+        self.parts = parts
+        self.repository = repository
